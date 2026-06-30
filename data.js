278af5d49a3d37dd4316e44f43a3d622f09cf6b7
amaaninteriorsvly-code <amaaninteriorsvly@gmail.com>
2026-06-30 12:21:15 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,131 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-view-1",
+      "name": "VIEW 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2712603221153256
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6101845738429983,
+          "pitch": 0.42241446318867304,
+          "rotation": 0,
+          "target": "1-view-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-view-2",
+      "name": "VIEW 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.015542849860089802,
+          "pitch": 0.3334349292031895,
+          "rotation": 0,
+          "target": "2-view-3"
+        },
+        {
+          "yaw": 3.107642268307096,
+          "pitch": 0.3572485359138131,
+          "rotation": 0,
+          "target": "0-view-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-view-3",
+      "name": "VIEW 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.09519632874931361,
+          "pitch": 0.48431198859943336,
+          "rotation": 0,
+          "target": "1-view-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "KHALIB EDUWIN EDIT123",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
